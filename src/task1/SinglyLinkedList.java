0341ad1d9a9bc2ca1dbfbe0e69360c821e3c1466
TeemuTallskog <72005659+TeemuTallskog@users.noreply.github.com>
2022-03-16 13:26:32 +0200

    new methods

--- src/task1/SinglyLinkedList.java
+++ src/task1/SinglyLinkedList.java
@@ -11,27 +11,82 @@ public class SinglyLinkedList {
 
     private Node tail = null;
 
-    private List<String> linkedList = new LinkedList<String>();
-
     public SinglyLinkedList(){
 
     }
 
+    private void headNull(Node node){
+        head = node;
+        tail = node;
+        currentPOS = node;
+    }
+
+    public void addToTail(String value){
+        Node node = new Node(value);
+        if(head == null){
+            headNull(node);
+        }else{
+            tail.next = node;
+        }
+        tail = node;
+    }
     public void addToNextPOS(String value){
         Node node = new Node(value);
         if(head == null){
-            head = node;
-        }
-        if(tail == null){
+            headNull(node);
+        }if(currentPOS.next == null){
+            tail.next = node;
             tail = node;
+            currentPOS.next = node;
+        }else{
+            node.next = currentPOS.next;
+            currentPOS.next = node;
         }
-        currentPOS.next = node;
+    }
+    public void addToPreviousPOS(String value){
+        if(head == null){
+            Node newNode = new Node(value);
+            headNull(newNode);
+        }
+        if(currentPOS == head){
+            Node newNode = new Node(value);
+            newNode.next = head;
+            head = currentPOS;
+        }else{
+            previousPOS();
+            addToNextPOS(value);
+        }
     }
 
 
     public void nextPOS(){
+        if(currentPOS.next == null){
+            currentPOS = head;
+            return;
+        }
         currentPOS = currentPOS.next;
     }
+    public void previousPOS(){
+        if(currentPOS == head){
+            return;
+        }
+        Node current = currentPOS;
+        currentPOS = head;
+        while(current != currentPOS.next){
+            nextPOS();
+        }
+    }
 
+    public void removeCurrentNode(){
+        previousPOS();
+        currentPOS.next = currentPOS.next.next;
+    }
+
+    public void moveToHead(){
+        currentPOS = head;
+    }
+    public void moveToTail(){
+        currentPOS = tail;
+    }
 
 }
